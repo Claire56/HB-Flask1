bd7c7b4de5789a371e4e0a703dfd8da2d88f7d60
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-22 14:39:49 -0800

    made changes

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Greeting Flask app."""
 
-from random import choice
-
+#from random import choice
+  
 from flask import Flask, request
 
 # "__name__" is a special Python variable for the name of the current module
@@ -18,7 +18,12 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html>
+    <html>Hi! This is the home page. 
+    <body>
+    <a href="http://localhost:5000/hello">Hello</a>
+    </body>
+    </html>"""
 
 
 @app.route("/hello")
@@ -35,6 +40,16 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          Select a compliment 
+          <select name="compliment">
+            <option value = 'awesome'>Awesome</option>
+            <option value = 'terrific'>Terrific</option>
+            <option value = 'fantastic'>Fantastic</option>
+            <option value = 'neato'>Neato</option>
+            <option value = 'fantabulous'>Antabulous</option>
+            <option value = 'wowza'>Wowza</option>
+            <option value = 'brilliant'>Brilliant</option>
+          </select>
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -48,9 +63,8 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
-
-    y = x
+    compliment = request.args.get("compliment")
+    
 
     return """
     <!doctype html>
@@ -68,4 +82,6 @@ def greet_person():
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
+
+
